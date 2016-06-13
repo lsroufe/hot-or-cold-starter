a317461bc382d36a5bee1261849459a2afc0b7ea
Logan Sroufe <lsroufe@gmail.com>
2016-06-13 10:05:56 -0700

    updated js

--- js/app.js
+++ js/app.js
@@ -11,11 +11,35 @@ $(document).ready(function() {
   		$(".overlay").fadeOut(1000);
   	});
 
+    var randomNumber;
+
   	var newGame = function(){
-  		var randomNumber = Math.floor(Math.random()*100)+1;
+      randomNumber = Math.floor(Math.random()*100)+1;
   		console.log(randomNumber);
   		console.log('hello world');
   	};
 
   	newGame();
+
+    $("a.new").click(function() {
+      newGame();
+    });
+
+
+  // on form submition - e.g. when the Guess button is pressed
+  $('form').submit(function(event){
+    event.preventDefault();
+
+    var submitNumber = $('.text').val();
+
+    console.log(submitNumber);
+
+    if (randomNumber == submitNumber) {
+      console.log("You win!");
+    } else if (Math.abs(randomNumber - submitNumber) >= 50) {
+      console.log("You're ice cold!");
+    }
+
+  });
+
 });
