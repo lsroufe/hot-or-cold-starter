afc59e844b6d95157123e3421153974b31dd5905
Logan Sroufe <lsroufe@gmail.com>
2016-06-20 09:51:50 -0700

    Updated app.js

--- js/app.js
+++ js/app.js
@@ -2,29 +2,33 @@
 $(document).ready(function() {
 
 	/*--- Display information modal box ---*/
-  	$(".what").click(function(){
-    	$(".overlay").fadeIn(1000);
-  	});
+	$(".what").click(function(){
+  	$(".overlay").fadeIn(1000);
+	});
 
-  	/*--- Hide information modal box ---*/
-  	$("a.close").click(function(){
-  		$(".overlay").fadeOut(1000);
-  	});
+	/*--- Hide information modal box ---*/
+	$("a.close").click(function(){
+		$(".overlay").fadeOut(1000);
+	});
 
-    var randomNumber;
+  var randomNumber;
+  var guessCount = 0;
 
-  	var newGame = function(){
-      randomNumber = Math.floor(Math.random()*100)+1;
-  		console.log(randomNumber);
-  		console.log('hello world');
-  	};
+	var newGame = function(){
+    randomNumber = Math.floor(Math.random()*100)+1;
+		console.log(randomNumber);
+		guessCount = 0;
+    $('#feedback').text("Make your guess!");
+    $('#count').text("0");
+    $('#guessList').empty();
+    $('#userGuess').val("");
+	};
 
-  	newGame();
+	newGame();
 
-    $("a.new").click(function() {
-      newGame();
-    });
-
+  $("a.new").click(function() {
+    newGame();
+  });
 
   // on form submition - e.g. when the Guess button is pressed
   $('form').submit(function(event){
@@ -35,10 +39,20 @@ $(document).ready(function() {
     console.log(submitNumber);
 
     if (randomNumber == submitNumber) {
-      console.log("You win!");
+      $('#feedback').text("You win!");
     } else if (Math.abs(randomNumber - submitNumber) >= 50) {
-      console.log("You're ice cold!");
+      $('#feedback').text("You're ice cold!");
+    } else if (Math.abs(randomNumber - submitNumber) >= 30) {
+      $('#feedback').text("You're cold!");
+    } else if (Math.abs(randomNumber - submitNumber) >= 20) {
+      $('#feedback').text("You're warm!");
+    } else if (Math.abs(randomNumber - submitNumber) >= 10) {
+      $('#feedback').text("You're really warm!");
     }
+
+    $('#count').text(++guessCount);
+
+    $('#guessList').append('<li>' + submitNumber + '</li>');
 
   });
 
